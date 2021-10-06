7060df04f5d8beb4d5fec1e08e9a76dd3839fb0a
unknown <bulgackoff.denis@yandex.ru>
2021-10-06 11:47:56 +0300

    arrayOfsheep

---
+++ JS/8/arrayOFsheep.js
@@ -0,0 +1,10 @@
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    let kol =0
+    for(let item of arrayOfSheep){
+        if(item === true){
+            kol ++
+        }
+    }
+    return kol
+  }
